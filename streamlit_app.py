8d151469f6cf2be36507870be2a8a592ee9c0721
eelgayar-Snow <135655287+eelgayar-Snow@users.noreply.github.com>
2023-06-18 08:54:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new Healty Dine')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omeg 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie)
+streamlit.text('Hard-Bolied Free Range Egg')
